0c7b2496867a2644a17ab1defd74c3f2f14fd787
brett hockridge <bretthockridge98@gmail.com>
2022-05-18 19:20:44 -0700

    make it better

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const db = require("./db/db.json");
-const { v1: uuidv1 } = require("uuid");
+const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
 let app = express();
 let PORT = process.env.port || 3001;
 
+var idCreate = () => {
+  return 'id-' + Math.random().toString(36).substring(2, 16);
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public"));
-
-var createId = () => {
-    return 'id-' + Math.random().toString(36).substring(2, 16);
-  };
 
 //   Home Page path
   app.get("/", (req, res) => {
@@ -26,8 +25,8 @@ var createId = () => {
   });
 
 //   Listed Notes
-  app.get("/api/notes", (req, res) => {
-      return res.json(db);
+  app.get("/notes", (req, res) => {
+      res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
   app.post("/api/notes", (req, res) => {
@@ -41,6 +40,27 @@ var createId = () => {
   app.get("*", (req, res) => {
       res.sendFile(path.join(_dirname, "./public/index.html"));
   });
+
+  app.post("/api/notes", (req,res) => {
+    let newNote = req.body;
+    newNote.id = idCreate();
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  let noteDelete = req.params.id;
+
+  for (let i = 0; i < notes.length; i++) {
+      if (noteDelete === notes[i].id) {
+          notes.splice(i,1);
+          fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+          return res.json(notes);
+      }
+  }
+  return res.json(false);
+})
 
   app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}`);
